39c171017a9ecf2c7a24fb25d0e1e42c2fb2cdde
SerafimArts <nesk@xakep.ru>
2025-05-27 15:13:54 +0000

    Apply linter issues

---
+++ tests/TestCase.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Boson\Bridge\Laravel\Http\Tests;
+
+use PHPUnit\Framework\Attributes\Group;
+use PHPUnit\Framework\TestCase as BaseTestCase;
+
+#[Group('boson-php/laravel-http-bridge')]
+abstract class TestCase extends BaseTestCase {}
